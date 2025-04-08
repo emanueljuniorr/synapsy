a22189389f0dbca91fe6c0078d335b3d3d194766
Sora <vetuschronos@hotmail.com>
2025-04-08 19:47:45 -0300

    Fix: Resolver erro de babel-loader migrando de Turbopack para Webpack

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
+  // Configuração para resolver problemas com arquivos JSON nas dependências
+  webpack: (config) => {
+    // Resolver problemas com arquivos JSON no react-syntax-highlighter
+    config.module.rules.push({
+      test: /\.json$/,
+      type: 'javascript/auto',
+      resolve: {
+        fullySpecified: false
+      }
+    });
+    
+    return config;
+  }
+};
+
+module.exports = nextConfig; 
