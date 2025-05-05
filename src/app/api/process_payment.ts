f82cfdb23746cca22244f1d365cafc7e2858e30d
Emanuel Júnior <emanueljunior579@gmail.com>
2025-05-05 17:16:23 -0300

    fix: firebase credentials not found

--- src/app/api/process_payment.ts
+++ src/app/api/process_payment.ts
@@ -7,7 +7,7 @@ import { authOptions } from '@/lib/auth';
 
 // Inicializar Stripe com a chave secreta
 const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-03-31.basil',
+  apiVersion: '2025-04-30.basil',
 });
 
 export async function POST(req: NextRequest) {
